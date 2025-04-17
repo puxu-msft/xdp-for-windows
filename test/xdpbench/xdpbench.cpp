eda1888c2b082d93e74de37783b386fd5b817724
huajianwang <huajianwang@microsoft.com>
2025-04-17 04:47:52 +0000

    MOD: Update for sending asap

--- test/xdpbench/xdpbench.cpp
+++ test/xdpbench/xdpbench.cpp
@@ -1,55 +1,7 @@
 //
 // Copyright (c) Microsoft Corporation.
 //
-
-#include <afxdp_helper.h>
-#include <afxdp_experimental.h>
-#include <xdpapi.h>
-
-#include <assert.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "OLSHighPerfTimer.h"
-
-#include <winsock2.h>
-#include <netiodef.h>
-#include <string>
-#include <vector>
-#include <ws2ipdef.h>
-
-#pragma warning(disable:4200) // nonstandard extension used: zero-sized array in struct/union
-
-#define SHALLOW_STR_OF(x) #x
-#define STR_OF(x) SHALLOW_STR_OF(x)
-
-#define ALIGN_DOWN_BY(length, alignment) \
-    ((ULONG_PTR)(length)& ~(alignment - 1))
-#define ALIGN_UP_BY(length, alignment) \
-    (ALIGN_DOWN_BY(((ULONG_PTR)(length)+alignment - 1), alignment))
-
-#define STRUCT_FIELD_OFFSET(structPtr, field) \
-    ((UCHAR *)&(structPtr)->field - (UCHAR *)(structPtr))
-
-#define DEFAULT_UMEM_SIZE 65536
-#define DEFAULT_UMEM_CHUNK_SIZE 4096
-#define DEFAULT_UMEM_HEADROOM 0
-#define DEFAULT_IO_BATCH 1
-#define DEFAULT_NODE_AFFINITY -1
-#define DEFAULT_GROUP -1
-#define DEFAULT_IDEAL_CPU -1
-#define DEFAULT_CPU_AFFINITY 0
-#define DEFAULT_UDP_DEST_PORT 0
-#define DEFAULT_DURATION ULONG_MAX
-#define DEFAULT_TX_IO_SIZE 64
-#define DEFAULT_LAT_COUNT 10000000
-#define DEFAULT_YIELD_COUNT 0
-
-//huajianwang:eelat
-#define DEFAULT_FRAMES_PER_FILE 1
-#define DEFAULT_FILE_RATE 0
-#define DEFAULT_FRAME_RATE 10000
-//-huajianwang:eelat
+#include "xdpbench.h"
 
 CHAR* HELP =
 "xskbench.exe <rx|tx|fwd|lat|down> -i <ifindex> [OPTIONS] <-t THREAD_PARAMS> [-t THREAD_PARAMS...] \n"
@@ -137,35 +89,6 @@ CHAR* HELP =
 "   xskbench.exe fwd -i 6 -t -q -id 0 -y\n"
 "   xskbench.exe lat -i 6 -t -q -id 0 -ring_size 8\n"
 ;
-
-#define printf_error(...) \
-    fprintf(stderr, __VA_ARGS__)
-
-#define printf_verbose(format, ...) \
-    if (verbose) { LARGE_INTEGER Qpc; QueryPerformanceCounter(&Qpc); printf("Qpc=%llu " format, Qpc.QuadPart, __VA_ARGS__); }
-
-#define ABORT(...) \
-    printf_error(__VA_ARGS__); exit(1)
-
-#define ASSERT_FRE(expr) \
-    if (!(expr)) { ABORT("(%s) failed line %d\n", #expr, __LINE__);}
-
-#if DBG
-#define VERIFY(expr) assert(expr)
-#else
-#define VERIFY(expr) (expr)
-#endif
-
-#define Usage() PrintUsage(__LINE__)
-
-#define WAIT_DRIVER_TIMEOUT_MS 1050
-#define STATS_ARRAY_SIZE 60
-
-INT64
-QpcToUs64(
-    INT64 Qpc,
-    INT64 QpcFrequency
-);
 
 /*
 void init_token_bucket(sTokenBucket* bucket, int init_capacity, int init_refill_rate) {
@@ -1342,7 +1265,7 @@ WriteTxPackets(
         Timestamp[1] = Queue->doneSamplesOnTxMode;
         Queue->sent++;
         //output
-        if (Queue->doneSamplesOnTxMode % 10000 == 0) {
+        if (Queue->filerate!=0 && Queue->doneSamplesOnTxMode % 10000 == 0) {
 		    printf("sending %lld samples\n", Queue->doneSamplesOnTxMode);
         }
         //-huajianwang:eelat
@@ -1438,7 +1361,10 @@ ProcessTx(
     }
 
     //huajianwang:eelat
-    UINT32 nextsent = min(Queue->iobatchsize, Queue->frameperfile - Queue->sent);
+    UINT32 nextsent = Queue->iobatchsize;
+	if (Queue->filerate != 0) {
+        nextsent = min(Queue->iobatchsize, Queue->frameperfile - Queue->sent);
+	}
     //available =
     //    RingPairReserve(
     //        &Queue->freeRing, &consumerIndex, &Queue->txRing, &producerIndex, Queue->iobatchsize);
